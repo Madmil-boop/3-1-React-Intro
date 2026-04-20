459750650efcd8a8dda4d31745b246df6ed6da21
Madmill-boop <mmiller827212@gmail.com>
2026-04-19 20:55:20 -0500

    done

---
+++ react-intro/src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Counter } from './counter.tsx'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      hello 
+    <Counter num={5} />
+    </>
+  )
+}
+
+export default App
---
+++ react-intro/src/counter.tsx
@@ -0,0 +1,8 @@
+export function Counter({num}: {num: number}) {
+  return (
+    <>
+      Your number is {num}
+
+    </>
+  )
+}
