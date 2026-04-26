a138dc91890888a789c7a0a50a318f8a1bbda72f
Madmill-boop <mmiller827212@gmail.com>
2026-04-26 17:09:34 -0500

    rendering

--- react-intro/src/App.tsx
+++ react-intro/src/App.tsx
@@ -1,14 +1,19 @@
 
-import { Counter } from './counter.tsx'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-
+let [counter, setCounter] = useState(0)
+useEffect(() => {window.alert("renderd!")});
+let staticVariable = 0
 
   return (
     <>
-      hello 
-    <Counter num={5} />
+      <p>Counter: {counter}</p>
+      <p>Static Variable: {staticVariable}</p>
+      <button onClick={() => setCounter(counter + 1)}>Increment Counter</button>
+      <button onClick={() => staticVariable++}>Increment Static Variable</button>
     </>
   )
 }
--- react-intro/src/counter.tsx
+++
@@ -1,8 +0,0 @@
-export function Counter({num}: {num: number}) {
-  return (
-    <>
-      Your number is {num}
-
-    </>
-  )
-}
